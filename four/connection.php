1b2c49a6d194b01d3c6a78ca3dc01aca6059a52f
JLOM324JR <radtanaporn.kn@gmail.com>
2018-05-08 23:37:02 +0700

    set_character

--- four/connection.php
+++ four/connection.php
@@ -5,5 +5,5 @@ if (!$connect) {
     # code...
     die("Error".mysqli_error());
 }
-
+mysqli_query($connect, "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
 ?>
