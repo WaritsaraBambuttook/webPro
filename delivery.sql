cbd3a1647c0ad3a5656818ba054cffe555caa182
WaritsaraBambuttook <four_nukiz@hotmail.com>
2018-05-09 20:39:57 +0700

    แก้ดาต้าเบส

--- delivery.sql
+++ delivery.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2018 at 09:36 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.4
+-- Generation Time: May 09, 2018 at 03:39 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,8 +32,20 @@ CREATE TABLE `dessert` (
   `DessertID` int(11) NOT NULL,
   `Dessert_name` varchar(30) NOT NULL,
   `PriceDessert` int(11) NOT NULL,
-  `DPicture` varchar(60) NOT NULL
+  `img` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `dessert`
+--
+
+INSERT INTO `dessert` (`DessertID`, `Dessert_name`, `PriceDessert`, `img`) VALUES
+(1, 'Smoothie', 69, 'image/dessert/21.jpg'),
+(2, 'Pancake', 79, 'image/dessert/22.jpg'),
+(3, 'Crepescake', 79, 'image/dessert/23.jpg'),
+(4, 'BrownieCake', 49, 'image/dessert/24.jpg'),
+(5, 'Strawberrycheesecake', 69, 'image/dessert/25.jpg'),
+(6, 'DonutCake', 59, 'image/dessert/26.jpg');
 
 -- --------------------------------------------------------
 
@@ -45,8 +57,20 @@ CREATE TABLE `food` (
   `FoodID` int(11) NOT NULL,
   `Food_name` varchar(30) NOT NULL,
   `PriceFood` int(11) NOT NULL,
-  `FPicture` varchar(60) NOT NULL
+  `img` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`FoodID`, `Food_name`, `PriceFood`, `img`) VALUES
+(1, 'HealthySalad', 79, 'image/food/11.jpg'),
+(2, 'Friedrice', 89, 'image/food/12.jpg'),
+(3, 'soup', 79, 'image/food/13.jpg'),
+(4, 'JapaneseCurryRice', 99, 'image/food/16.jpg'),
+(5, 'Mixedsalad', 79, 'image/food/14.jpg'),
+(6, 'Ricetopped ', 69, 'image/food/15.jpg');
 
 -- --------------------------------------------------------
 
@@ -84,13 +108,22 @@ INSERT INTO `login` (`Login_ID`, `Firstname`, `Lastname`, `Telephone`, `Email`, 
 CREATE TABLE `report` (
   `ReportID` int(11) NOT NULL,
   `Product_name` varchar(30) NOT NULL,
-  `Customer_Fname` varchar(30) NOT NULL,
-  `Customer_Lname` varchar(30) NOT NULL,
-  `Address_Customer` varchar(30) NOT NULL,
-  `Telephone_Customer` int(11) NOT NULL,
-  `ProductID` int(11) NOT NULL,
-  `CustomerID` int(11) NOT NULL
+  `Price` int(11) NOT NULL,
+  `Count` int(11) NOT NULL,
+  `Total` int(11) NOT NULL,
+  `Login_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `report`
+--
+
+INSERT INTO `report` (`ReportID`, `Product_name`, `Price`, `Count`, `Total`, `Login_ID`) VALUES
+(59, 'BrownieCake', 49, 1, 49, 100007),
+(60, 'BrownieCake', 49, 1, 49, 100007),
+(61, 'DonutCake', 59, 1, 59, 100007),
+(62, 'BrownieCake', 49, 1, 49, 100007),
+(63, 'Ricetopped ', 69, 10, 690, 100007);
 
 --
 -- Indexes for dumped tables
@@ -120,7 +153,8 @@ ALTER TABLE `login`
 -- Indexes for table `report`
 --
 ALTER TABLE `report`
-  ADD PRIMARY KEY (`ReportID`);
+  ADD PRIMARY KEY (`ReportID`),
+  ADD KEY `Login_ID` (`Login_ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -130,19 +164,35 @@ ALTER TABLE `report`
 -- AUTO_INCREMENT for table `dessert`
 --
 ALTER TABLE `dessert`
-  MODIFY `DessertID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `DessertID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `FoodID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `FoodID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
   MODIFY `Login_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100008;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `ReportID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`Login_ID`) REFERENCES `login` (`Login_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
